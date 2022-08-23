f720c97da33447dfff1ed851106f89e1a244ea92
Can.Ru <8016667hao000>
2022-08-23 11:33:09 +0800

    swagger修复

--- src/main/java/com/example/CommunityApplication.java
+++ src/main/java/com/example/CommunityApplication.java
@@ -2,7 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -10,6 +20,18 @@ public class CommunityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CommunityApplication.class, args);
+    }
+
+
+    @Bean
+    public Docket createRestApi(){
+        ApiInfo DEFAULT = new ApiInfo("Api community demo", "Api community demo", "1.0", "urn:tos",
+                new Contact("Jianing.Zhang", "x:8080", "x@email.com"), "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
+                .paths(PathSelectors.any()).build();
+
     }
 
 }
